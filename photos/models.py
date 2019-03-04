60e7004ccf766fc6d71f117b0be18fa7b117ab7c
iankabugi <iank299@gmail.com>
2019-03-04 15:01:29 +0300

    add an image model and  add all the properties you are supposed to have

--- photos/models.py
+++ photos/models.py
@@ -3,46 +3,45 @@ import datetime as dt
 
 
 # Create your models here.
-class Photographer(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=10, blank=True)
-
-    def __str__(self):
-        return self.first_name
-
-    def save_editor(self):
-        self.save()
-
-
-# class tags(models.Model):
-#     name = models.CharField(max_length=30)
+# class Photographer(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     email = models.EmailField()
+#     phone_number = models.CharField(max_length=10, blank=True)
 
 #     def __str__(self):
-#         return self.name
+#         return self.first_name
 
+#     def save_photographer(self):
+#         self.save()
 
-# class Image(models.Model):
-#     image = models.ImageField(upload_to = 'images/')
-#     title = models.CharField(max_length=60)
-#     photographer = models.ForeignKey(Photographer)
-#     tags = models.ManyToManyField(tags)
-#     pub_date = models.DateTimeField(auto_now_add=True)
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+    def save_category(self):
+        self.save()
+
+class Location(models.Model):
+    location = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
+
+    def save_category(self):
+        self.save()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        photos = cls.objects.filter(title__icontains=search_term)
+        return photos
     
-    
-#     @classmethod
-#     def todays_images(cls):
-#         today = dt.date.today()
-#         images = cls.objects.filter(pub_date__date=today)
-#         return news
-
-#     @classmethod
-#     def days_images(cls, date):
-#         images = cls.objects.filter(pub_date__date=date)
-#         return images
-
-#     @classmethod
-#     def search_by_title(cls, search_term):
-#         images = cls.objects.filter(title__icontains=search_term)
-#         return iamges
