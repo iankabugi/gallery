977d5d4e209c60c21882ff4e1922c5bb12a03941
iankabugi <iank299@gmail.com>
2019-03-04 15:02:20 +0300

    configure database to postgresql in the settings python file

--- gallery/settings.py
+++ gallery/settings.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'ian',
+        'PASSWORD':'sawasawa',
+    }
+}
 
 
 # Password validation
