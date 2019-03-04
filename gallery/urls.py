3a4d83a1193ee6b1dae288a0ae52e29c4a9df180
iankabugi <iank299@gmail.com>
2019-03-04 18:18:59 +0300

    change routing to home

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'photos/',include('photos.urls')),
+    url(r'',include('photos.urls')),
     
 ]
