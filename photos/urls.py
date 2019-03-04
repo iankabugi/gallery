60d9a341be6fc520c5d85587366610acf965796c
iankabugi <iank299@gmail.com>
2019-03-04 18:27:07 +0300

    configure the app to allow for images to be displayed

--- photos/urls.py
+++ photos/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
 urlpatterns = [
     url('^$', views.welcome, name='welcome'),
     # landing page
-    url('^$',views.photos,name = 'photos'),
-    #search results page 
+    url('^$', views.photos, name='photos'),
+    # search results page
     url(r'^search/', views.search_results, name='search_results'),
     # link to the image details
-    url(r'^image/(\d+)',views.image,name='image')
+    url(r'^image/(\d+)', views.image, name='image')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
