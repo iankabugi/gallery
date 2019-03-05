1d4378cd8228d684c203104d2a1a987d1c3812f3
iankabugi <iank299@gmail.com>
2019-03-05 10:47:16 +0300

    configure the routing of the page accordingly

--- photos/urls.py
+++ photos/urls.py
@@ -4,13 +4,12 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url('^$', views.welcome, name='welcome'),
     # landing page
     url('^$', views.photos, name='photos'),
     # search results page
     url(r'^search/', views.search_results, name='search_results'),
-    # link to the image details
-    url(r'^image/(\d+)', views.image, name='image')
+    url(r"^page/",views.page, name = "location"),
+    url(r"^location/(\w+)",views.location, name = "locations")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
