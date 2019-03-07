f8e485b2076150fbd3c7f24e476f6bbd61360578
iankabugi <45152886+iankabugi@users.noreply.github.com>
2019-03-07 12:39:26 +0300

    update tests

--- photos/tests.py
+++ photos/tests.py
@@ -67,7 +67,7 @@ class ImageTestCase(TestCase):
         lagos = Location(location="Nairobi")
         lagos.save()
         self.new_image = Image(
-            name="image", description="h", location=Nairobi, category=wildlife)
+            name="image", description="this is a unique image", location=Nairobi, category=wildlife)
 
     def tearDown(self):
         """
