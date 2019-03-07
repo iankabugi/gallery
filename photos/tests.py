331b4f44f4f8f881413a6a22158b91f013a18446
iankabugi <iank299@gmail.com>
2019-03-07 11:19:16 +0300

    added tests to the application

--- photos/tests.py
+++ photos/tests.py
@@ -1,5 +1,104 @@
 from django.test import TestCase
-from .models import Image
+from .models import Image, Category, Location
 import datetime as dt
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class CategoryTestClass(TestCase):
+ # Set up method
+    def setUp(self):
+        self.type = Category(category='wildlife')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.type, Category))
+
+    def test_init(self):
+
+        self.assertTrue(self.type.category == 'wildlife')
+
+    def test_save_method(self):
+        self.type.save_category()
+        categorys = Category.objects.all()
+        self.assertTrue(len(categorys) > 0)
+
+    def test_delete_method(self):
+        self.type.save_category()
+        categorys = Category.objects.all()
+        self.type.delete_category()
+        categorys = Category.objects.all()
+        self.assertTrue(len(categorys) == 0)
+
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.place = Location(location='nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.place, Location))
+
+    def test_init(self):
+        self.assertTrue(self.place.location == 'nairobi')
+
+    def test_save_method(self):
+        self.place.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_delete_method(self):
+        self.place.save_location()
+        locations = Location.objects.all()
+        self.place.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
+
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        """
+        This will create a new image before each test case
+        """
+        fun = Category(category="wildlife")
+        fun.save()
+        lagos = Location(location="Nairobi")
+        lagos.save()
+        self.new_image = Image(
+            name="image", description="h", location=Nairobi, category=wildlife)
+
+    def tearDown(self):
+        """
+        This will clear the db after each test
+        """
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+
+    def test_instance(self):
+
+        self.assertTrue(isinstance(self.new_image, Image))
+
+    def test_save_image(self):
+
+        self.new_image.save_image()
+        self.assertTrue(len(Image.objects.all()) > 0)
+
+    def test_filter_by_location(self):
+
+        self.new_image.save_image()
+        self.assertTrue(len(Image.filter_by_location("Nairobi")) > 0)
+
+    def test_delete_method(self):
+        self.new_image.save_image()
+        images = Image.objects.all()
+        self.new_image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_search_by_category(self):
+
+        self.new_image.save_image()
+        self.assertTrue(len(Image.search_by_category("wildlife")) > 0)
